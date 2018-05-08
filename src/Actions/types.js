9d0e1af6fe06bb1fb79881b54333ab235e334df1
Anil Choudhary <choudharya335@gmail.com>
2018-05-08 11:39:11 +0530

    conflict resolved( todos, todoAction) 08-052018

--- src/Actions/types.js
+++ src/Actions/types.js
@@ -3,3 +3,7 @@ export const ADD_TODO = 'ADD_TODO';
 export const GET_TODOS = 'GET_TODOS';
 export const GET_TODOS_SUCCESS = 'GET_TODOS_SUCCESS';
 export const GET_TODOS_FAIL = 'GET_TODOS_FAIL';
+
+export const POST_TODO = 'POST_TODO';
+export const POST_TODO_SUCCESS = 'POST_TODO_SUCCESS';
+export const POST_TODO_FAIL = 'POST_TODO_FAIL';
